a64dc920b745daaf952ced4fdb6d7ed336dd2696
Gordie Smith <agsmith4@gmail.com>
2014-12-23 01:56:14 +0000

    Finish user edit, update, index and destroy actions.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionController::TestCase
   def setup
     @user = users(:michael)
-    @other_user = users(:sterling)
+    @other_user = users(:archer)
   end
   
   test "should get new" do
@@ -40,6 +40,21 @@ class UsersControllerTest < ActionController::TestCase
   test "should redirect index when not logged in" do
     get :index
     assert_redirected_to login_url
+  end
+  
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to login_url
+  end
+  
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to root_url
   end
 
 end
