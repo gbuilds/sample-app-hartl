306a7e0d0f7f4c5a39308fbf016033287d43df93
Gordie Smith <agsmith4@gmail.com>
2015-01-04 20:03:01 +0000

    Add test for User feed

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -77,4 +77,22 @@ class UserTest < ActiveSupport::TestCase
     assert_not michael.following?(archer)
   end
   
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    lana = users(:lana)
+    archer = users(:archer)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    # Posts from self
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    # Posts from an unfollowed user
+    archer.microposts.each do |posts_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
+  
 end
